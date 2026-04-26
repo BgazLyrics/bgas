77ad8596f32db21505eaedf20b8641f8eda4e02b
Bagas Seviardana Rionaldi <139362025+BgazLyrics@users.noreply.github.com>
2026-04-26 20:04:32 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,9 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <meta name="google-site-verification" content="iHXiimogguhYXMQ7-3sExWZWFTx2D7o_R5xxcmeU99A" />
       <Navbar />
       <main className="relative z-10 w-full flex flex-col overflow-hidden">
+      <meta name="google-site-verification" content="iHXiimogguhYXMQ7-3sExWZWFTx2D7o_R5xxcmeU99A" />
         <Hero />
         <About />
         <Skills />
